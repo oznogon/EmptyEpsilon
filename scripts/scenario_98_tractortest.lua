da8d7e36f8804a0b77216eeb00df28d69483f9e3
oznogon <oznogon@users.noreply.github.com>
2025-06-10 00:14:10 -0700

    Add test ship, scenario

---
+++ scripts/scenario_98_tractortest.lua
@@ -0,0 +1,166 @@
+-- Name: Tractor test
+-- Description: Pull
+-- Type: Development
+
+--- Scenario
+-- @script scenario_98_tractor_test
+
+function init()
+	ECS = false
+	if createEntity then
+		ECS = true
+	end
+    SpaceStation():setPosition(1000, 1000):setTemplate('Small Station'):setFaction("Human Navy"):setRotation(random(0, 360))
+    --SpaceStation():setPosition(-1000, 1000):setTemplate('Medium Station'):setFaction("Human Navy"):setRotation(random(0, 360))
+    --SpaceStation():setPosition(1000, -1000):setTemplate('Large Station'):setFaction("Human Navy"):setRotation(random(0, 360))
+    --SpaceStation():setPosition(-1000, -1000):setTemplate('Huge Station'):setFaction("Human Navy"):setRotation(random(0, 360))
+    player1 = PlayerSpaceship():setFaction("Human Navy"):setTemplate("Heracles"):setRotation(200):setBeamWeaponDamageType(0, "emp")
+
+    --player2 = PlayerSpaceship():setFaction("Human Navy"):setTemplate("Atlantis"):setRotation(0)
+    --Nebula():setPosition(-5000, 0)
+    Artifact():setPosition(1000, 9000):setModel("small_frigate_1"):setDescription(_("scienceDescription-artifact", "An old space derelict."))
+    Artifact():setPosition(9000, 2000):setModel("small_frigate_1"):setDescription(_("scienceDescription-artifact", "A wrecked ship."))
+    Artifact():setPosition(3000, 4000):setModel("small_frigate_1"):setDescription(_("scienceDescription-artifact", "Tons of rotting plasteel."))
+    --addGMFunction(_("buttonGM", "move 1 to 2"), function() player1:transferPlayersToShip(player2) end)
+    --addGMFunction(_("buttonGM", "move 2 to 1"), function() player2:transferPlayersToShip(player1) end)
+    CpuShip():setTemplate("Adder MK5"):setPosition(0, 0):setRotation(0):setFaction("Human Navy")
+    --CpuShip():setTemplate("Piranha F12"):setPosition(2000, 0):setRotation(-90):setFaction("Kraylor")
+    for n = 1, 1000 do
+        Asteroid():setPosition(random(-50000, 50000), random(-50000, 50000)):setSize(random(100, 500))
+        VisualAsteroid():setPosition(random(-50000, 50000), random(-50000, 50000)):setSize(random(100, 500))
+    end
+
+    addGMFunction(
+        _("buttonGM", "Random asteroid field"),
+        function()
+            cleanup()
+            for n = 1, 1000 do
+                Asteroid():setPosition(random(-50000, 50000), random(-50000, 50000)):setSize(random(100, 500))
+                VisualAsteroid():setPosition(random(-50000, 50000), random(-50000, 50000)):setSize(random(100, 500))
+            end
+        end
+    )
+    addGMFunction(
+        _("buttonGM", "Random nebula field"),
+        function()
+            cleanup()
+            for n = 1, 50 do
+                Nebula():setPosition(random(-50000, 50000), random(-50000, 50000))
+            end
+        end
+    )
+    addGMFunction(
+        _("buttonGM", "Delete unselected"),
+        function()
+            local gm_selection = getGMSelection()
+            for idx, obj in ipairs(getAllObjects()) do
+                local found = false
+                for idx2, obj2 in ipairs(gm_selection) do
+                    if obj == obj2 then
+                        found = true
+                    end
+                end
+                if not found then
+                    obj:destroy()
+                end
+            end
+        end
+    )
+end
+
+function isObjectType(obj,typ,qualifier)
+	if obj ~= nil and obj:isValid() then
+		if typ ~= nil then
+			if ECS then
+				if typ == "SpaceStation" then
+					return obj.components.docking_bay and obj.components.physics and obj.components.physics.type == "static"
+				elseif typ == "PlayerSpaceship" then
+					return obj.components.player_control
+				elseif typ == "ScanProbe" then
+					return obj.components.allow_radar_link
+				elseif typ == "CpuShip" then
+					return obj.ai_controller
+				elseif typ == "Asteroid" then
+					return obj.components.mesh_render and string.sub(obj.components.mesh_render.mesh, 7) == "Astroid"
+				elseif typ == "Nebula" then
+					return obj.components.nebula_renderer
+				elseif typ == "Planet" then
+					return obj.components.planet_render
+				elseif typ == "SupplyDrop" then
+					return obj.components.pickup and obj.components.radar_trace.icon == "radar/blip.png" and obj.components.radar_trace.color_by_faction
+				elseif typ == "BlackHole" then
+					return obj.components.gravity and obj.components.billboard_render.texture == "blackHole3d.png"
+				elseif typ == "WarpJammer" then
+					return obj.components.warp_jammer
+				elseif typ == "Mine" then
+					return obj.components.delayed_explode_on_touch and obj.components.constant_particle_emitter
+				elseif typ == "EMPMissile" then
+					return obj.components.radar_trace.icon == "radar/missile.png" and obj.components.explode_on_touch.damage_type == "emp"
+				elseif typ == "Nuke" then
+					return obj.components.radar_trace.icon == "radar/missile.png" and obj.components.explosion_sfx == "sfx/nuke_explosion.wav"
+				elseif typ == "Zone" then
+					return obj.components.zone
+				else
+					if qualifier == "MovingMissile" then
+						if typ == "HomingMissile" or typ == "HVLI" or typ == "Nuke" or typ == "EMPMissile" then
+							return obj.components.radar_trace.icon == "radar/missile.png"
+						else
+							return false
+						end
+					elseif qualifier == "SplashMissile" then
+						if typ == "Nuke" or typ == "EMPMissile" then
+							if obj.components.radar_trace.icon == "radar/missile.png" then
+								if typ == "Nuke" then
+									return obj.components.explosion_sfx == "sfx/nuke_explosion.wav"
+								else	--EMP
+									return obj.components.explode_on_touch.damage_type == "emp"
+								end
+							else
+								return false
+							end
+						else
+							return false
+						end
+					else
+						return false
+					end
+				end
+			else
+				return obj.typeName == typ
+			end
+		else
+			return false
+		end
+	else
+		return false
+	end
+end
+
+function cleanup()
+    -- Clean up the current play field. Find all objects and destroy everything that is not a player.
+    -- If it is a player, position him in the center of the scenario.
+    for idx, obj in ipairs(getAllObjects()) do
+        if isObjectType(obj,"PlayerSpaceship") then
+            obj:setPosition(random(-100, 100), random(-100, 100))
+        else
+            obj:destroy()
+        end
+    end
+end
+
+function update(delta)
+    -- No victory condition
+end
+
+-- Set callback function
+onNewPlayerShip(
+    function(ship)
+        -- Decide what you do with new ships:
+        if ECS then
+        	print(ship, ship:getTypeName(), ship:getCallSign())
+        else
+	        print(ship, ship.typeName, ship:getTypeName(), ship:getCallSign())
+	    end
+        -- ship:destroy()
+    end
+)
---
+++ scripts/shiptemplates/tug.lua
@@ -0,0 +1,52 @@
+--[[                  Tug
+Tugs are small ships with a powerful reactor and tractor beam, and few or no weapons.
+They rely on carrier ships or space stations, and are invaluable for recovering or salvaging disabled, abandoned, or derelict ships.
+Tugs deployed into hazardous areas might have shields, and those tasked with salvaging partially operational ships might have shield-draining beams or EMP launchers. 
+----------------------------------------------------------]]
+
+local template = ShipTemplate():setName("Hylas"):setLocaleName(_("ship", "Hylas")):setClass(_("class", "Tug")):setModel("battleship_destroyer_1_upgraded")
+template:setDescription(_([[The Hylas is the smallest functional model of tug. Its powerful reactor is almost entirely dedicated to its tractor beam, leaving only minimal impulse engines to slowly move it and the objects it tows.]]))
+template:setRadarTrace("tug.png")
+template:setHull(40)
+template:setSpeed(10, 2.5, 4)
+template:setTractorBeam(18, 180, 0, 3000, 5000, 6.0, 1.0)
+
+local variation = template:copy("Hylas"):setName("Heracles"):setLocaleName(_("playerShip", "Heracles")):setType("playership")
+variation:setDescription(_([[The Heracles tug model is a Hylas suited for carrier use in deep-space operations. Improvements include a stronger hull, minimal shield system, full sensor suite, and a shield-disrupting beam for use on abandoned hostile or unknown ships.]]))
+variation:setShields(100)
+variation:setHull(100)
+variation:setBeamWeapon(0, 90, 0, 1000, 2, 1)
+
+variation:addRoomSystem(1, 0, 2, 1, "Maneuver");
+variation:addRoomSystem(1, 1, 2, 1, "BeamWeapons");
+variation:addRoom(2, 2, 2, 1);
+
+variation:addRoomSystem(0, 3, 1, 2, "RearShield");
+variation:addRoomSystem(1, 3, 2, 2, "Reactor");
+variation:addRoomSystem(3, 3, 2, 2, "Warp");
+variation:addRoomSystem(5, 3, 1, 2, "JumpDrive");
+variation:addRoomSystem(6, 3, 2, 1, "TractorBeam");
+variation:addRoom(6, 4, 2, 1);
+variation:addRoomSystem(8, 3, 1, 2, "FrontShield");
+
+variation:addRoom(2, 5, 2, 1);
+variation:addRoomSystem(1, 6, 2, 1, "MissileSystem");
+variation:addRoomSystem(1, 7, 2, 1, "Impulse");
+
+variation:addDoor(1, 1, true);
+variation:addDoor(2, 2, true);
+variation:addDoor(3, 3, true);
+variation:addDoor(1, 3, false);
+variation:addDoor(3, 4, false);
+variation:addDoor(3, 5, true);
+variation:addDoor(2, 6, true);
+variation:addDoor(1, 7, true);
+variation:addDoor(5, 3, false);
+variation:addDoor(6, 3, false);
+variation:addDoor(6, 4, false);
+variation:addDoor(8, 3, false);
+variation:addDoor(8, 4, false);
+
+--Airlock doors
+--variation:addDoor(2, 2, false);
+--variation:addDoor(2, 5, false);
