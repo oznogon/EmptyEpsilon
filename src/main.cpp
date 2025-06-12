e1f58a787f08b76eb0685ceab8d0d8dedfa14224
Elliot Drees <elliot@trellick.net>
2025-06-12 18:33:10 -0400

    Added the default setting in main for now

--- src/main.cpp
+++ src/main.cpp
@@ -238,6 +238,10 @@ int main(int argc, char** argv)
         new ShipSelectionScreen();
     }
 
+    if (PreferencesManager::get("joystick_deadzone").empty())
+        PreferencesManager::set("joystick_deadzone", "0.1");
+    sp::io::Keybinding::setDeadzone(PreferencesManager::get("joystick_deadzone").toFloat());
+
     engine->runMainLoop();
 
     // Set FSAA and fullscreen defaults from windowManager.
@@ -259,9 +263,6 @@ int main(int argc, char** argv)
     if (PreferencesManager::get("engine_enabled").empty())
         PreferencesManager::set("engine_enabled", "2");
     
-    if (PreferencesManager::get("joystick_deadzone").empty())
-        PreferencesManager::set("joystick_deadzone", "0.1");
-    sp::io::Keybinding::setDeadzone(PreferencesManager::get("joystick_deadzone").toFloat());
     
 
     if (PreferencesManager::get("headless") == "")
