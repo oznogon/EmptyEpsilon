a41ee276434855bd39e1ddc3f426c3cba41eb70f
oznogon <oznogon@users.noreply.github.com>
2025-07-14 14:53:50 -0700

    Dream a little dream

--- src/gameGlobalInfo.cpp
+++ src/gameGlobalInfo.cpp
@@ -375,18 +375,81 @@ string GameGlobalInfo::getMissionTime() {
 
 string getSectorName(glm::vec2 position)
 {
-    constexpr float sector_size = 20000;
-    int sector_x = floorf(position.x / sector_size) + 5;
-    int sector_y = floorf(position.y / sector_size) + 5;
-    string y;
-    string x;
-    if (sector_y >= 0)
-        if (sector_y < 26)
-            y = string(char('A' + (sector_y)));
-        else
-            y = string(char('A' - 1 + (sector_y / 26))) + string(char('A' + (sector_y % 26)));
-    else
-        y = string(char('z' + ((sector_y + 1) / 26))) + ((sector_y  % 26) == 0 ? "a" : string(char('z' + 1 + (sector_y  % 26))));
-    x = string(sector_x);
-    return y + x;
+    constexpr float subsector_size = 20000.0f;
+    constexpr float sector_size = subsector_size * 5.0f;
+    constexpr float supersector_size = sector_size * 5.0f;
+
+    /*  Subsectors are 20000 by 20000 square.
+        Sectors are 5 subsectors by 5 subsectors square.
+        Supersectors are 5 sectors by 5 sectors square.
+        Subsectors are 1-indexed, row-first.
+        Sectors are alphabetically indexed, row-first.
+        Supersectors are alphabetically indexed, row-first.
+
+        if position.x = 1000 and position.y = 10000
+            subsector index should be 1 (top-left)
+            sector index should be A (top-left subsector in sector)
+            supersector index should be A (sector A is top-left sector in supersector)
+            label should be A:A1
+
+        if position.x = 21000 and position.y = 21000
+            subsector index should be 7 (1 right and 1 down from top-left)
+            sector index should be A (same sector as A:A1)
+            supersector index should be A (same supersector A:A1)
+            label should be A:A7
+
+        if position.x = 501000 and position.y = 501000
+            subsector index should be 1 (top-left subsector)
+            sector index should be A (subsector 1 is top-left subsector in sector)
+            supersector index should be B (1 right and 0 down from top-left)
+            label should be B:A1
+
+        ------------------------- 500000 wide ------------------------- | -- next supersector starts
+        -------- 100000 wide --------
+        A:A01 A:A02 A:A03 A:A04 A:A05 ... A:E01 A:E02 A:E03 A:E04 A:E05 | B:A01 ...
+        A:A06 A:A07 A:A08 A:A09 A:A10 ... A:E06 A:E07 A:E08 A:E09 A:E10 | B:A06 ...
+        A:A11 A:A12 A:A13 A:A14 A:A15 ... A:E11 A:E12 A:E13 A:E14 A:E15 | B:A11 ...
+        A:A16 A:A17 A:A18 A:A19 A:A20 ... A:E16 A:E17 A:E18 A:E19 A:E20 | B:A16 ...
+        A:A21 A:A22 A:A23 A:A24 A:A25 ... A:E21 A:E22 A:E23 A:E24 A:E25 | B:A21 ...
+        ...                          A:M13                          ... | ...
+        A:U01 A:U02 A:U03 A:U04 A:U05 ... A:Y01 A:Y02 A:Y03 A:Y04 A:Y05 | B:U01 ...
+        A:U06 A:U07 A:U08 A:U09 A:U10 ... A:Y06 A:Y07 A:Y08 A:Y09 A:Y10 | B:U06 ...
+        A:U11 A:U12 A:U13 A:U14 A:U15 ... A:Y11 A:Y12 A:Y13 A:Y14 A:Y15 | B:U11 ...
+        A:U16 A:U17 A:U18 A:U19 A:U20 ... A:Y16 A:Y17 A:Y18 A:Y19 A:Y20 | B:U16 ...
+        A:U21 A:U22 A:U23 A:U24 A:U25 ... A:Y21 A:Y22 A:Y23 A:Y24 A:Y25 | B:U21 ...
+
+        equivalent to integer supersector indices:
+
+        -------------------- 500000 wide --------------------- | -- next supersector starts
+        ----- 100000 wide ------ .... ----- 100000 wide ------ |
+        0001 0002 0003 0004 0005 .... 0021 0022 0023 0024 0025 | 0626 
+        0026 0027 0028 0029 0030 .... 0046 0047 0048 0049 0050 | ...
+row 1   0051 0052 0053 0054 0055 .... 0071 0072 0073 0074 0075 | ...
+        0076 0077 0078 0079 0080 .... 0096 0097 0098 0099 0100 | ...
+        0101 0102 0103 0104 0105 .... 0121 0122 0123 0124 0125 | ...
+rows 2-4 ...                     0313                          | ... 
+        0501 0502 0503 0504 0505 .... 0521 0522 0523 0524 0025 | ...
+        0526 0527 0528 0529 0530 .... 0546 0547 0548 0549 0050 | ...
+row 5   0551 0552 0553 0554 0555 .... 0571 0572 0573 0574 0075 | ...
+        0576 0577 0578 0579 0580 .... 0596 0597 0598 0599 0600 | ...
+        0601 0602 0603 0604 0605 .... 0621 0622 0623 0624 0625 | ... 1250
+
+        Supersectors (M is "home" and typically lacks prefix)
+        ---------------------------- 12.5m wide -----------------------------
+                                     |2.5m wide|
+rows 1-2                                 ...
+        KA KB KC KD KE|LA LB LC LD LE|A B C D E|NA NB NC ND NE|OA OB OC OD OE
+        KF KG KH KI KJ|LF LG LH LI LJ|F G H I J|NF NG NH NI NJ|OF OG OH OI OJ
+row 3   KK KL KM KN KO|LK LL LM LN LO|K L(M)N O|NK NL NM NN NO|OK OL OM ON OO
+        KP KQ KR KS KT|LP LQ LR LS LT|P Q R S T|NP NQ NR NS NT|OP OQ OR OS OT
+        KU KV KW KX KY|LU LV LW LX LY|U V W X Y|NU NV NW NX NY|OU OV OW OX OY
+rows 4-5                                 ...
+
+(top-left of A:A1 should be engine -6250000,-6250000; further prepends lower-z backwards)
+(center of M:M13 should be engine 0,0)
+(bottom-right of Y:Y25 should be engine +6250000,+6250000; further prepends upper-Z backwards)
+
+*/
+    // return supersector_index + ":" + sector_index + string(subsector_index);
+    return "";
 }
