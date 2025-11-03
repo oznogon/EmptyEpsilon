d9db23f67f6c6e50f5edf4ba7115eca4503da66d
Oznogon <garrett+ozgh@oznogon.com>
2025-11-03 13:52:21 -0800

    Parameterize GuiRotatingModelView

--- src/screenComponents/rotatingModelView.h
+++ src/screenComponents/rotatingModelView.h
@@ -1,5 +1,4 @@
-#ifndef ROTATING_MODEL_VIEW_H
-#define ROTATING_MODEL_VIEW_H
+#pragma once
 
 #include "gui/gui2_element.h"
 #include "components/rendering.h"
@@ -8,10 +7,16 @@ class GuiRotatingModelView : public GuiElement
 {
 private:
     sp::ecs::Entity &entity;
+    float zoom_factor;
+    float angle;
 public:
     GuiRotatingModelView(GuiContainer* owner, string id, sp::ecs::Entity& entity);
 
     virtual void onDraw(sp::RenderTarget& target) override;
-};
 
-#endif//ROTATING_MODEL_VIEW_H
+    void setZoomFactor(float new_factor) { zoom_factor = std::max(0.1f, new_factor); }
+    float getZoomFactor() { return zoom_factor; }
+    bool is_rotating;
+    void setAngle(float new_angle);
+    float getAngle() { return angle; }
+};
