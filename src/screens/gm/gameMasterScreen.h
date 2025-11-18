9f62dccfe4f3442526b1bac5e6b0e242f9996fc6
Oznogon <garrett+ozgh@oznogon.com>
2025-11-18 12:39:49 -0800

    Add time scale selector to GM screen
    Add a GuiSelector to select a time scale for the game, ranging from
    1x (the default) to 16x.

--- src/screens/gm/gameMasterScreen.h
+++ src/screens/gm/gameMasterScreen.h
@@ -53,6 +53,7 @@ private:
     GuiButton* player_comms_hail;
     GuiButton* global_message_button;
     GuiToggleButton* pause_button;
+    GuiSelector* game_time_scale;
     GuiToggleButton* intercept_comms_button;
     GuiButton* tweak_button;
     GuiRadarZoomSlider* zoom_slider;
