9ae54fda2730c756f84797dc58e0ffaa8196d4db
oznogon <oznogon@users.noreply.github.com>
2025-12-22 23:05:09 -0800

    Remove powerDamp

--- src/math/damp.h
+++ src/math/damp.h
@@ -10,30 +10,6 @@
 // Frame-rate independent damping using linear interpolation
 // https://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
 
-// Power-based, where speed is between 0 and 1
-static inline float powerDamp(const float& source, const float& target, float speed, float delta)
-{
-    return glm::lerp(source, target, 1.0f - std::pow(speed, delta));
-}
-
-static inline glm::vec2 powerDamp(const glm::vec2& source, const glm::vec2& target, float speed, float delta)
-{
-    return glm::lerp(source, target, 1.0f - std::pow(speed, delta));
-}
-
-static inline glm::vec3 powerDamp(const glm::vec3& source, const glm::vec3& target, float speed, float delta)
-{
-    return glm::lerp(source, target, 1.0f - std::pow(speed, delta));
-}
-
-static inline float powerAngleDamp(const float& source, const float& target, float speed, float time_delta)
-{
-    float angle_delta = std::fmod((target - source), 360.0f);
-    if (angle_delta > 180.0f) angle_delta -= 360.0f;
-    else if (angle_delta < -180.0f) angle_delta += 360.0f;
-    return glm::lerp(source, source + angle_delta, 1.0f - std::pow(speed, time_delta));
-}
-
 // Exponential decay-based, where speed influences convergence rate
 static inline float exponentialDamp(const float& source, const float& target, float speed, float delta)
 {
