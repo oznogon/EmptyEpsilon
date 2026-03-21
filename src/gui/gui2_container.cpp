900d90173f76bdaac2246be4404ba2a1a0f77b87
oznogon <oznogon@users.noreply.github.com>
2026-03-21 12:29:59 -0700

    Reimplement recursive descendent scrolling in GuiContainer
    Remove use of SP-based translation/reprojection and implement
    scrolling by recursively manipulating descendant rects.
    
    - Add GuiContainer::offsetElementRect() to recursively apply the
      given positional offset.
    - Replace SP translation methods in GuiScrollContainer with
      GuiContainer::offsetElementRect().
    - Update GuiSelector popup handling.

--- src/gui/gui2_container.cpp
+++ src/gui/gui2_container.cpp
@@ -161,6 +161,12 @@ GuiElement* GuiContainer::callExecuteScrollOnElement(GuiContainer* container, gl
     return container->executeScrollOnElement(pos, value);
 }
 
+void GuiContainer::offsetElementRect(GuiContainer* container, glm::vec2 offset)
+{
+    container->rect.position += offset;
+    for (auto* child : container->children)
+        offsetElementRect(child, offset);
+}
 
 void GuiContainer::setAttribute(const string& key, const string& value)
 {
