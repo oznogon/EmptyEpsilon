e426dce6f959fed626e3d762dc8bba6211601288
Oznogon <garrett+ozgh@oznogon.com>
2026-04-02 16:41:14 -0700

    Fix potential null ref, minor refactor of fighterAI
    - Simplify canSwitchAI conditional.
    - Consolidate target_physics->getSize() checks on Dive state.
    - Prevent potential null ref in dive state if an entity has_missiles
      but no longer has a MissileTubes component.

--- src/ai/fighterAI.cpp
+++ src/ai/fighterAI.cpp
@@ -16,31 +16,24 @@ FighterAI::FighterAI(sp::ecs::Entity owner)
 : ShipAI(owner)
 {
     attack_state = State::Dive;
-    timeout = 0.0;
-    aggression = random(0.0, 0.25);
+    timeout = 0.0f;
+    aggression = random(0.0f, 0.25f);
 }
 
 bool FighterAI::canSwitchAI()
 {
-    if (owner.hasComponent<Target>() && (has_missiles || has_beams))
-    {
-        if (attack_state == State::Dive)
-            return false;
-    }
-    return true;
+    return !(owner.hasComponent<Target>() && (has_missiles || has_beams) && attack_state == State::Dive);
 }
 
 void FighterAI::run(float delta)
 {
-    if (timeout > 0.0f)
-        timeout -= delta;
+    if (timeout > 0.0f) timeout -= delta;
     ShipAI::run(delta);
 }
 
 void FighterAI::runOrders()
 {
-    if (aggression > 0.5f)
-        aggression -= random(0.0, 0.25);
+    if (aggression > 0.5f) aggression -= random(0.0f, 0.25f);
     ShipAI::runOrders();
 }
 
@@ -50,6 +43,7 @@ void FighterAI::runAttack(sp::ecs::Entity target)
     if (!transform) return;
     auto tt = target.getComponent<sp::Transform>();
     if (!tt) return;
+
     auto position_diff = tt->getPosition() - transform->getPosition();
     float distance = glm::length(position_diff);
     auto shields = owner.getComponent<Shields>();
@@ -58,65 +52,63 @@ void FighterAI::runAttack(sp::ecs::Entity target)
     switch(attack_state)
     {
     case State::Dive:
-        if (distance < 2500 + (target_physics ? target_physics->getSize().x : 0.0f) && has_missiles)
+    {
+        auto target_size = target_physics ? std::max(target_physics->getSize().x, target_physics->getSize().y) : 0.0f;
+        if (distance < 2500.0f + target_size && has_missiles)
         {
-            auto tubes = owner.getComponent<MissileTubes>();
-            for(auto& tube : tubes->mounts)
+            if (auto tubes = owner.getComponent<MissileTubes>())
             {
-                if (tube.state == MissileTubes::MountPoint::State::Loaded && missile_fire_delay <= 0.0f)
+                for (auto& tube : tubes->mounts)
                 {
-                    float target_angle = calculateFiringSolution(target, tube);
-                    if (target_angle != std::numeric_limits<float>::infinity())
+                    if (tube.state == MissileTubes::MountPoint::State::Loaded && missile_fire_delay <= 0.0f)
                     {
-                        MissileSystem::fire(owner, tube, target_angle, target);
-                        missile_fire_delay = tube.load_time / tubes->mounts.size() / 2.0f;
+                        const float target_angle = calculateFiringSolution(target, tube);
+                        if (target_angle != std::numeric_limits<float>::infinity())
+                        {
+                            MissileSystem::fire(owner, tube, target_angle, target);
+                            missile_fire_delay = tube.load_time / tubes->mounts.size() / 2.0f;
+                        }
                     }
                 }
             }
         }
 
-        flyTowards(tt->getPosition(), 500.0);
+        flyTowards(tt->getPosition(), 500.0f);
 
-        if (distance < 500 + (target_physics ? target_physics->getSize().x : 0.0f))
+        if (distance < 500.0f + target_size)
         {
-            aggression += random(0, 0.05);
+            aggression += random(0.0f, 0.05f);
 
             attack_state = State::Evade;
             timeout = 30.0f - std::min(aggression, 1.0f) * 20.0f;
 
             float target_dir = vec2ToAngle(position_diff);
             float a_diff = angleDifference(target_dir, transform->getRotation());
-            if (a_diff < 0)
-                evade_direction = target_dir - random(25, 40);
-            else
-                evade_direction = target_dir + random(25, 40);
+            if (a_diff < 0.0f) evade_direction = target_dir - random(25.0f, 40.0f);
+            else evade_direction = target_dir + random(25.0f, 40.0f);
         }
+
         if (shields && !shields->entries.empty() && shields->entries[0].level < shields->entries[0].max * (1.0f - aggression))
         {
             attack_state = State::Recharge;
-            aggression += random(0.1, 0.25);
+            aggression += random(0.1f, 0.25f);
             timeout = 60.0f - std::min(aggression, 1.0f) * 20.0f;
         }
         break;
+    }
     case State::Evade:
-        if (distance > 2500 || timeout <= 0.0f)
-        {
-            attack_state = State::Dive;
-        }
+        if (distance > 2500.0f || timeout <= 0.0f) attack_state = State::Dive;
         else
         {
-            auto thrusters = owner.getComponent<ManeuveringThrusters>();
-            if (thrusters) thrusters->target = evade_direction;
-            auto impulse = owner.getComponent<ImpulseEngine>();
-            if (impulse)
-                impulse->request = 1.0;
+            if (auto thrusters = owner.getComponent<ManeuveringThrusters>()) thrusters->target = evade_direction;
+            if (auto impulse = owner.getComponent<ImpulseEngine>()) impulse->request = 1.0f;
         }
         break;
     case State::Recharge:
         if ((shields && !shields->entries.empty() && shields->entries[0].level < shields->entries[0].max * 0.9f) || timeout <= 0.0f)
-        {
             attack_state = State::Dive;
-        }else{
+        else
+        {
             auto target_position = tt->getPosition();
             float circle_distance = 3000.0f;
             target_position += vec2FromAngle(vec2ToAngle(target_position - transform->getPosition()) + 170.0f) * circle_distance;
