fc3a6e384a24fc3486c6f245055db03443af9ba5
Oznogon <garrett+ozgh@oznogon.com>
2026-04-05 16:03:07 -0700

    Add options for waypoint routes, sets
    Add options to allow waypoints to be connected by lines to form
    routes, and for a player ship to manage up to 4 sets of waypoints.
    
    Waypoint sets provide both additional waypoints and means to
    differentiate waypoints. Each set has a theme-defined color and waypoint
    icon, defaulting to the EE default icon and the default blue-gray (set 1),
    red (2), green (3), and cyan blue (4).
    
    Waypoint routes connect all waypoints in a set with a line, in ascending
    ID order.
    
    If these options are enabled, this adds controls to Relay and Operations
    to select a waypoint set when adding or selecting waypoints, and to toggle
    a waypoint set's route visibility.
    
    Existing Lua functions for waypoint management can add, remove, and
    reposition waypoints in each set by passing the set ID number as an
    optional last argument. This also adds a Lua function to enable or
    disable route behavior (commandSetWaypointRoute(bool)).
    
    These options are NOT ENABLED by default. This PR should NOT CHANGE
    default behaviors for players or scenario writers. This PR adds toggles
    for these options to the "Extra settings" menu available from the ship
    selection screen, making them OPT-IN at the server level.

--- src/playerInfo.cpp
+++ src/playerInfo.cpp
@@ -107,6 +107,7 @@ static const uint16_t CMD_CUSTOM_FUNCTION = 0x0029;
 static const uint16_t CMD_TURN_SPEED = 0x002A;
 static const uint16_t CMD_CREW_SET_TARGET = 0x002B;
 static const uint16_t CMD_ABORT_JUMP = 0x002C;
+
 static const uint16_t CMD_LAUNCH_INTERNAL = 0x002D;
 static const uint16_t CMD_MOVE_INTERNAL_TO_BERTH = 0x002E;
 static const uint16_t CMD_SET_BERTH_TRANSFER_DIRECTION = 0x002F;
@@ -115,11 +116,13 @@ static const uint16_t CMD_TRANSFER_PROBE = 0x0031;
 static const uint16_t CMD_GENERATE_SUPPLY_DROP = 0x0032;
 static const uint16_t CMD_CANCEL_INTERNAL_MOVE = 0x0033;
 
-static const uint16_t CMD_SET_UTILITY_BEAM = 0x002D;
-static const uint16_t CMD_SET_CUSTOM_UTILITY_BEAM_MODE = 0x002F;
-static const uint16_t CMD_SET_UTILITY_BEAM_BEARING = 0x0030;
-static const uint16_t CMD_SET_UTILITY_BEAM_ARC = 0x0031;
-static const uint16_t CMD_SET_UTILITY_BEAM_RANGE = 0x0032;
+static const uint16_t CMD_SET_UTILITY_BEAM = 0x0034;
+static const uint16_t CMD_SET_CUSTOM_UTILITY_BEAM_MODE = 0x0035;
+static const uint16_t CMD_SET_UTILITY_BEAM_BEARING = 0x0036;
+static const uint16_t CMD_SET_UTILITY_BEAM_ARC = 0x0037;
+static const uint16_t CMD_SET_UTILITY_BEAM_RANGE = 0x0038;
+
+static const uint16_t CMD_SET_WAYPOINT_ROUTE = 0x0039;
 
 //Pre-ship commands
 static const uint16_t CMD_UPDATE_CREW_POSITION = 0x0101;
@@ -467,24 +470,31 @@ void PlayerInfo::commandSetShieldFrequency(int32_t frequency)
     sendClientCommand(packet);
 }
 
-void PlayerInfo::commandAddWaypoint(glm::vec2 position)
+void PlayerInfo::commandAddWaypoint(glm::vec2 position, int32_t set_id)
 {
     sp::io::DataBuffer packet;
-    packet << CMD_ADD_WAYPOINT << position;
+    packet << CMD_ADD_WAYPOINT << position << set_id;
     sendClientCommand(packet);
 }
 
-void PlayerInfo::commandRemoveWaypoint(int32_t index)
+void PlayerInfo::commandRemoveWaypoint(int32_t index, int32_t set_id)
 {
     sp::io::DataBuffer packet;
-    packet << CMD_REMOVE_WAYPOINT << index;
+    packet << CMD_REMOVE_WAYPOINT << index << set_id;
     sendClientCommand(packet);
 }
 
-void PlayerInfo::commandMoveWaypoint(int32_t index, glm::vec2 position)
+void PlayerInfo::commandMoveWaypoint(int32_t index, glm::vec2 position, int32_t set_id)
 {
     sp::io::DataBuffer packet;
-    packet << CMD_MOVE_WAYPOINT << index << position;
+    packet << CMD_MOVE_WAYPOINT << index << position << set_id;
+    sendClientCommand(packet);
+}
+
+void PlayerInfo::commandSetWaypointRoute(bool is_route, int32_t set_id)
+{
+    sp::io::DataBuffer packet;
+    packet << CMD_SET_WAYPOINT_ROUTE << is_route << set_id;
     sendClientCommand(packet);
 }
 
@@ -1183,19 +1193,21 @@ void PlayerInfo::onReceiveClientCommand(int32_t client_id, sp::io::DataBuffer& p
     case CMD_ADD_WAYPOINT:
         {
             glm::vec2 position{};
-            packet >> position;
+            int32_t set_id = 1;
+            packet >> position >> set_id;
             auto wp = ship.getComponent<Waypoints>();
             if (wp) {
-                wp->addNew(position);
+                wp->addNew(position, set_id);
             }
         }
         break;
     case CMD_REMOVE_WAYPOINT:
         {
             int32_t id;
-            packet >> id;
+            int32_t set_id = 1;
+            packet >> id >> set_id;
             if (auto wp = ship.getComponent<Waypoints>()) {
-                wp->remove(id);
+                wp->remove(id, set_id);
             }
         }
         break;
@@ -1203,9 +1215,20 @@ void PlayerInfo::onReceiveClientCommand(int32_t client_id, sp::io::DataBuffer& p
         {
             int32_t id;
             glm::vec2 position{};
-            packet >> id >> position;
+            int32_t set_id = 1;
+            packet >> id >> position >> set_id;
             if (auto wp = ship.getComponent<Waypoints>()) {
-                wp->move(id, position);
+                wp->move(id, position, set_id);
+            }
+        }
+        break;
+    case CMD_SET_WAYPOINT_ROUTE:
+        {
+            bool is_route;
+            int32_t set_id = 1;
+            packet >> is_route >> set_id;
+            if (auto wp = ship.getComponent<Waypoints>()) {
+                wp->setRoute(is_route, set_id);
             }
         }
         break;
