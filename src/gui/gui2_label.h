a306ef4d44104368bee1ed3d47f96c26d772d590
Oznogon <garrett+ozgh@oznogon.com>
2026-04-14 22:38:05 -0700

    Add functions to return the rendered height of a GuiLabel

--- src/gui/gui2_label.h
+++ src/gui/gui2_label.h
@@ -27,6 +27,9 @@ public:
     GuiLabel* setVertical();
     GuiLabel* setWrapped();
     GuiLabel* setClipped();
+
+    float getRenderedHeight() const;
+    float getRenderedHeight(float width) const;
 };
 
 class GuiAutoSizeLabel : public GuiLabel
