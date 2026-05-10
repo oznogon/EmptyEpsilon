0543e3bf8f4b94cce2883062f0607ca49f6aeee2
oznogon <oznogon@users.noreply.github.com>
2026-05-10 01:17:24 -0700

    Further clamp shield.hit_effect to 0+

--- src/systems/shieldsystem.cpp
+++ src/systems/shieldsystem.cpp
@@ -53,7 +53,7 @@ void ShieldSystem::update(float delta)
             }
 
             if (shield.hit_effect > 0.0f)
-                shield.hit_effect -= delta;
+                shield.hit_effect = std::max(0.0f, shield.hit_effect - delta);
             else
                 shield.hit_effect = 0.0f;
             n++;
