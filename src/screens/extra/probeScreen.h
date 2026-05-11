0b6f25dc0299d923d900426d366160a40bdf43e9
oznogon <oznogon@users.noreply.github.com>
2026-05-11 12:29:18 -0700

    Add mousedown ProbeScreen left/right rotation

--- src/screens/extra/probeScreen.h
+++ src/screens/extra/probeScreen.h
@@ -12,9 +12,12 @@ private:
     GuiImage* background_gradient;
     GuiViewport3D* viewport;
     GuiLabel* no_probe_label;
+    float mouse_turn_direction = 0.0f;
 public:
     ProbeScreen(GuiContainer* owner);
 
     virtual void onUpdate() override;
     virtual void onDraw(sp::RenderTarget& target) override;
+    virtual bool onMouseDown(sp::io::Pointer::Button button, glm::vec2 position, sp::io::Pointer::ID id) override;
+    virtual void onMouseUp(glm::vec2 position, sp::io::Pointer::ID id) override;
 };
