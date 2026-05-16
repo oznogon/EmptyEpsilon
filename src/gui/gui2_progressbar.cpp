5354cad70f166709f4e452e283d1afb3bbf29f5c
oznogon <oznogon@users.noreply.github.com>
2026-05-16 11:10:23 -0700

    Set text size on GuiProgressbar

--- src/gui/gui2_progressbar.cpp
+++ src/gui/gui2_progressbar.cpp
@@ -1,7 +1,6 @@
 #include "gui2_progressbar.h"
 #include "theme.h"
 
-
 GuiProgressbar::GuiProgressbar(GuiContainer* owner, string id, float min_value, float max_value, float start_value)
 : GuiElement(owner, id), min_value(min_value), max_value(max_value), value(start_value), color(glm::u8vec4(255, 255, 255, 64)), drawBackground(true)
 {
@@ -20,18 +19,19 @@ void GuiProgressbar::onDraw(sp::RenderTarget& renderer)
         renderer.drawStretched(rect, back.texture, back.color);
 
     sp::Rect fill_rect = rect;
+    const float size = text_size > 0.0f ? text_size : front.size;
     if (rect.size.x >= rect.size.y)
     {
         fill_rect.size.x *= f;
         if (max_value < min_value)
             fill_rect.position.x = rect.position.x + rect.size.x - fill_rect.size.x;
-        renderer.drawStretchedHVClipped(rect, fill_rect, front.size, front.texture, color);
+        renderer.drawStretchedHVClipped(rect, fill_rect, size, front.texture, color);
     }
     else
     {
         fill_rect.size.y *= f;
         fill_rect.position.y = rect.position.y + rect.size.y - fill_rect.size.y;
-        renderer.drawStretchedHVClipped(rect, fill_rect, front.size, front.texture, color);
+        renderer.drawStretchedHVClipped(rect, fill_rect, size, front.texture, color);
     }
     renderer.drawText(rect, text, sp::Alignment::Center);
 }
@@ -49,9 +49,16 @@ GuiProgressbar* GuiProgressbar::setRange(float min_value, float max_value)
     return this;
 }
 
-GuiProgressbar* GuiProgressbar::setText(string text)
+GuiProgressbar* GuiProgressbar::setText(string text, float size)
 {
+    this->text_size = size > 0.0f ? size : front_style->get(getState()).size;
     this->text = text;
+    return this;
+}
+
+GuiProgressbar* GuiProgressbar::setTextSize(float size)
+{
+    this->text_size = size > 0.0f ? size : front_style->get(getState()).size;
     return this;
 }
 
--- src/gui/gui2_progressbar.h
+++ src/gui/gui2_progressbar.h
@@ -1,16 +1,16 @@
-#ifndef GUI2_PROGRESSBAR_H
-#define GUI2_PROGRESSBAR_H
+#pragma once
 
 #include "gui2_element.h"
 
-
 class GuiThemeStyle;
+
 class GuiProgressbar : public GuiElement
 {
 protected:
     float min_value;
     float max_value;
     float value;
+    float text_size = 0.0f;
     glm::u8vec4 color;
     bool drawBackground;
     const GuiThemeStyle* back_style;
@@ -24,9 +24,8 @@ public:
 
     GuiProgressbar* setValue(float value);
     GuiProgressbar* setRange(float min_value, float max_value);
-    GuiProgressbar* setText(string text);
+    GuiProgressbar* setText(string text, float size = 0.0f);
+    GuiProgressbar* setTextSize(float size);
     GuiProgressbar* setColor(glm::u8vec4 color);
     GuiProgressbar* setDrawBackground(bool drawBackground);
 };
-
-#endif//GUI2_PROGRESSBAR_H
