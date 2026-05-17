c1c5c96c1dc4a94fb404d0f69eccb2bd9cfc3e7e
oznogon <oznogon@users.noreply.github.com>
2026-05-17 09:28:38 -0700

    [fork] Fix perpetual merge issues with packet consts

--- src/playerInfo.cpp
+++ src/playerInfo.cpp
@@ -34,6 +34,7 @@
 #include "screenComponents/selfDestructEntry.h"
 
 #include "components/ai.h"
+#include "components/analysisTarget.h"
 #include "components/beamweapon.h"
 #include "components/collision.h"
 #include "components/coolant.h"
@@ -135,32 +136,31 @@ static const uint16_t CMD_SET_AI_ORDER = 0x003C;
 static const uint16_t CMD_DRONE_DOCK = 0x003D;
 static const uint16_t CMD_DRONE_UNDOCK = 0x003E;
 static const uint16_t CMD_DRONE_ABORT_DOCK = 0x003F;
-static const uint16_t CMD_PROBE_TARGET_ROTATION = 0x0040;
 
 // Science/Target analysis commands
-static const uint16_t CMD_SET_ANALYSIS_TARGET = 0x0041;
+static const uint16_t CMD_SET_ANALYSIS_TARGET = 0x0040;
 
 // Docking bay commands
-static const uint16_t CMD_LAUNCH_INTERNAL = 0x0042;
-static const uint16_t CMD_MOVE_INTERNAL_TO_BERTH = 0x0043;
-static const uint16_t CMD_SET_BERTH_TRANSFER_DIRECTION = 0x0044;
-static const uint16_t CMD_TRANSFER_MISSILE = 0x0045;
-static const uint16_t CMD_TRANSFER_PROBE = 0x0046;
-static const uint16_t CMD_GENERATE_SUPPLY_DROP = 0x0047;
-static const uint16_t CMD_CANCEL_INTERNAL_MOVE = 0x0048;
+static const uint16_t CMD_LAUNCH_INTERNAL = 0x0041;
+static const uint16_t CMD_MOVE_INTERNAL_TO_BERTH = 0x0042;
+static const uint16_t CMD_SET_BERTH_TRANSFER_DIRECTION = 0x0043;
+static const uint16_t CMD_TRANSFER_MISSILE = 0x0044;
+static const uint16_t CMD_TRANSFER_PROBE = 0x0045;
+static const uint16_t CMD_GENERATE_SUPPLY_DROP = 0x0046;
+static const uint16_t CMD_CANCEL_INTERNAL_MOVE = 0x0047;
 
 // Utility beam commands
-static const uint16_t CMD_SET_UTILITY_BEAM = 0x0049;
-static const uint16_t CMD_SET_CUSTOM_UTILITY_BEAM_MODE = 0x004A;
-static const uint16_t CMD_SET_UTILITY_BEAM_BEARING = 0x004B;
-static const uint16_t CMD_SET_UTILITY_BEAM_ARC = 0x004C;
-static const uint16_t CMD_SET_UTILITY_BEAM_RANGE = 0x004D;
+static const uint16_t CMD_SET_UTILITY_BEAM = 0x0048;
+static const uint16_t CMD_SET_CUSTOM_UTILITY_BEAM_MODE = 0x0049;
+static const uint16_t CMD_SET_UTILITY_BEAM_BEARING = 0x004A;
+static const uint16_t CMD_SET_UTILITY_BEAM_ARC = 0x004B;
+static const uint16_t CMD_SET_UTILITY_BEAM_RANGE = 0x004C;
 
 // Waypoint commands
-static const uint16_t CMD_SET_WAYPOINT_ROUTE = 0x004E;
+static const uint16_t CMD_SET_WAYPOINT_ROUTE = 0x004D;
 
 // Probe control commands
-static const uint16_t CMD_PROBE_TARGET_ROTATION = 0x004F;
+static const uint16_t CMD_PROBE_TARGET_ROTATION = 0x004E;
 
 // Pre-ship commands
 static const uint16_t CMD_UPDATE_CREW_POSITION = 0x0101;
