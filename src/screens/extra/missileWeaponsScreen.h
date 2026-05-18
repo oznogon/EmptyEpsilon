0b8fff35474b0106c0b38b0226cb2cdb0e97973b
oznogon <oznogon@users.noreply.github.com>
2026-05-17 22:53:57 -0700

    Don't show radar on MissileWeaponsScreen if there's no MissileTubes or mounts

--- src/screens/extra/missileWeaponsScreen.h
+++ src/screens/extra/missileWeaponsScreen.h
@@ -6,18 +6,23 @@
 
 class AimLock;
 class AimLockButton;
+class GuiImage;
 class GuiKeyValueDisplay;
+class GuiLabel;
 class GuiMissileTubeControls;
 class GuiRadarView;
 
 class MissileWeaponsScreen : public GuiOverlay
 {
 private:
+    GuiImage* background_gradient;
     GuiOverlay* background_crosses;
+    GuiElement* missile_controls;
+    GuiLabel* no_weapons_label;
 
+    GuiRadarView* radar;
     TargetsContainer targets;
     GuiKeyValueDisplay* energy_display;
-    GuiRadarView* radar;
     GuiMissileTubeControls* tube_controls;
     AimLock* missile_aim;
     AimLockButton* lock_aim;
