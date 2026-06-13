75b2452642c67c2bed7374c5184a92a7be1b80af
oznogon <oznogon@users.noreply.github.com>
2026-06-13 10:10:16 -0700

    Track GL_SCISSOR_TEST state in GuiRenderedModelSprite
    To avoid corruption related to other scissor toggles, track
    GL_SCISSOR_TEST state in GuiRenderedModelSprite.
    
    Also clean up code and comments, and move initialized values to the
    header.

--- src/screenComponents/renderedModelSprite.h
+++ src/screenComponents/renderedModelSprite.h
@@ -10,12 +10,11 @@ class GuiRenderedModelSprite : public GuiElement
 private:
     sp::ecs::Entity &entity;
     std::unique_ptr<sp::RenderTexture> render_texture;
-    float zoom_factor;
-    float height;
-    float angle;
-    bool needs_rerender;
-    glm::ivec2 last_render_size;
-
+    float zoom_factor = 1.0f;
+    float height = -1.0f;
+    float angle = 90.0f;
+    bool needs_rerender = true;
+    glm::ivec2 last_render_size{0, 0};
 public:
     GuiRenderedModelSprite(GuiContainer* owner, string id, sp::ecs::Entity& entity);
 
@@ -27,7 +26,6 @@ public:
     float getCameraRotation() { return angle; }
     void setCameraHeight(float new_height);
     float getCameraHeight() { return height; }
-
 private:
     bool renderToTexture(glm::ivec2 size);
 };
