0769a25866733f64b6ba676a929df95a996362ea
oznogon <oznogon@users.noreply.github.com>
2026-06-26 16:46:01 -0700

    [fork] Pulse and stretch AlertOverlay background

--- src/screenComponents/alertOverlay.cpp
+++ src/screenComponents/alertOverlay.cpp
@@ -1,6 +1,8 @@
 #include "alertOverlay.h"
 #include "playerInfo.h"
 #include "gui/theme.h"
+#include "engine.h"
+#include "tween.h"
 
 AlertLevelOverlay::AlertLevelOverlay(GuiContainer* owner)
 : GuiElement(owner, "")
@@ -17,8 +19,7 @@ void AlertLevelOverlay::onDraw(sp::RenderTarget& renderer)
     if (!pc) return;
 
     glm::u8vec4 color;
-
-    switch(pc->alert_level)
+    switch (pc->alert_level)
     {
     case AlertLevel::RedAlert:
         color = glm::u8vec4(255, 0, 0, 255);
@@ -31,5 +32,17 @@ void AlertLevelOverlay::onDraw(sp::RenderTarget& renderer)
         return;
     }
 
-    renderer.drawSprite(alert_sprite, getCenterPoint(), 1000.0f, color);
+    // Pulse alert background transparency.
+    const float t = fmodf(engine->getElapsedTime(), PULSE_PERIOD);
+    const float half_period = PULSE_PERIOD * 0.5f;
+    float progress;
+
+    if (t < half_period)
+        progress = Tween<float>::easeInOutSine(t, 0.0f, half_period, 0.0f, 1.0f);
+    else
+        progress = 1.0f - Tween<float>::easeInOutSine(t - half_period, 0.0f, half_period, 0.0f, 1.0f);
+
+    color.a = static_cast<uint8_t>((0.25f + progress * 0.5f) * 255.0f);
+
+    renderer.drawStretched(rect, alert_sprite, color);
 }
